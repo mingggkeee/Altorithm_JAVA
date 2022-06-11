4387892c62867e317d851fe23278b9f1f00e4cd7
mingggkeee <mksu234@naver.com>
2022-06-11 22:23:52 +0900

    2022-06-11

---
+++ 구현/BOJ_2743.java
@@ -0,0 +1,16 @@
+package 구현;
+
+import java.util.Scanner;
+
+public class BOJ_2743 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String input = sc.next();
+		
+		System.out.println(input.length());
+		
+		sc.close();
+	}
+	
+}
